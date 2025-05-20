44a7f1737e3787df09f051ca8712b852bed07687
Maria <146016427+mgfuentes-ct@users.noreply.github.com>
2025-05-19 18:16:26 -0600

    Actividad 3

---
+++ e3.js
@@ -0,0 +1,8 @@
+// crea un arrow function llamada saludoPersonalizado que 
+// reciba dos parametros: nombre y edad, y retorne una cadena como la siguiente: "hola, me llamo Isay y tengo 37 años.
+
+const saludoPersonalizado = (nombre, edad) => "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+
+console.log(saludoPersonalizado("Isay", 37)); 
+console.log(saludoPersonalizado("Toiler", 24));
+console.log(saludoPersonalizado("Maria", 20));

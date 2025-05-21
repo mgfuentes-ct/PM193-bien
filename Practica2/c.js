1ac0e120d217c8fc462e9e9d2225d9fd479eb78e
Maria <146016427+mgfuentes-ct@users.noreply.github.com>
2025-05-20 23:25:18 -0600

    ejercicio c terminado

--- Practica2/c.js
+++ Practica2/c.js
@@ -6,3 +6,18 @@ const personas =[
 
 // mi codigo
 
+
+//1
+const personabuscar = personas.find(persona => persona.nombre === "Luis");
+console.log("Persona: ",personabuscar);
+
+
+//2
+personas.forEach(persona => {
+    console.log("Nombre: ",persona.nombre, "Edad: ",persona.edad);
+    }
+);
+
+//3
+const totalEdad = personas.reduce((suma, persona) => suma + persona.edad, 0);
+console.log("Total de edad: ",totalEdad);

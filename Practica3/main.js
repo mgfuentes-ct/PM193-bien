f13dfe81cc1de049fcb8b2b94d7ba61faca6689a
Maria <146016427+mgfuentes-ct@users.noreply.github.com>
2025-05-23 01:43:31 -0600

    Ejercicio A terminado

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,2 +1,8 @@
 //Ejercicio A
 
+const { restar } = require('./utils.js');
+
+console.log(`5 - 3 = ${restar(5, 3)}`);
+console.log(`10 - 4 = ${restar(10, 4)}`);
+console.log(`20 - 15 = ${restar(20, 15)}`);
+

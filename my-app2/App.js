4145c44e49b8cc9f88e1049e0471a4cdd7194d2d
Maria <146016427+mgfuentes-ct@users.noreply.github.com>
2025-06-12 16:07:21 -0600

    Primera parte de la practica 4

--- my-app2/App.js
+++ my-app2/App.js
@@ -1,15 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* Zona 1: lugar de las importaciones*/
 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto = (props) => {
+  const {contenido} = props;
+  return (
+    <Text>{contenido}</Text>
+  );
+}
+
+
+/* Zona 2: Es un tipo Main (zona de ejecución) */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> 
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="Mundo"></Texto>
+      <Texto contenido="ReactNative"></Texto>
+      <Texto contenido=":)"></Texto>
+      <Button title='Presionar'></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* Zona 3: lugar de los estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
